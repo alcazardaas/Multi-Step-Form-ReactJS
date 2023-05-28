7453e301e8c1753810fc0fe8034e0043fee694a5
Dylan <dylana@criticalmass.com>
2023-05-28 02:46:16 +0100

    Add pagination and change styles

--- src/components/Table/SummaryTable.js
+++ src/components/Table/SummaryTable.js
@@ -1,14 +1,27 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import TablePagination from '@mui/material/TablePagination';
 import Paper from '@mui/material/Paper';
 import dayjs from 'dayjs';
 
 export default function SummaryTable({ data, columns }) {
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   const isDate = (date) => {
     return dayjs(date).isValid()
@@ -18,28 +31,44 @@ export default function SummaryTable({ data, columns }) {
     return dayjs(date).format('DD/MM/YYYY')
   }
 
+  function validateStringToInt(str) {
+    const num = parseInt(str, 10);
+    return !isNaN(num);
+  }
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {columns.map((column, index) => (
-              <TableCell key={index}>{column.name}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((item, index) => (
-            <TableRow key={index}>
-              {columns.map((column, columnIndex) => (
-                <TableCell key={columnIndex}>
-                  {isDate(item[column.property]) ? formatDate(item[column.property]) : item[column.property]}
-                </TableCell>
+    <Paper>
+      <TableContainer>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column, index) => (
+                <TableCell key={index}>{column.name}</TableCell>
               ))}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((item, index) => (
+              <TableRow key={index}>
+                {columns.map((column, columnIndex) => (
+                  <TableCell key={columnIndex}>
+                    {validateStringToInt(item[column.property]) ? item[column.property] : isDate(item[column.property]) ? formatDate(item[column.property]) : item[column.property]}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
 }
